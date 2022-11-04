ed90bf5c760cc2ca03da81fc937642e4ada2b468
Zhou Zicai <zzc2152351@163.com>
2022-11-04 19:25:46 +0800

    更新了主页

--- jkflask/config.py
+++ jkflask/config.py
@@ -1,9 +1,9 @@
 import os
 HOSTNAME = '127.0.0.1'
 PORT = '3306'
-DATABASE = 'flaskdb'
+DATABASE = 'jk'
 USERNAME = 'root'
-PASSWORD = '001234'
+PASSWORD = 'zzc.030227'
 DB_URI = 'mysql+pymysql://{}:{}@{}:{}/{}?charset=utf8'.format(USERNAME, PASSWORD, HOSTNAME, PORT, DATABASE)
 SQLALCHEMY_DATABASE_URI = DB_URI
 SQLALCHEMY_TRACK_MODIFICATIONS = True
--- jkvue/src/views/home/home.vue
+++ jkvue/src/views/home/home.vue
@@ -1,7 +1,10 @@
 <template>
+    
     <div class="home_board">
+        <link rel="stylesheet" href="http://at.alicdn.com/t/font_1309180_m0vigzfu7y.css"/>
         <form class="search_form">
-            <span id="logo">Logo</span>
+            <div class="back"></div>
+            <img id="logo" src="../../picture/mylogo.png"/>
             <!--搜索区域可以大一点-->
             <button class="tag_button" v-on:click="show_tag_list">
                 <!--标签筛选-->
@@ -14,10 +17,15 @@
                     </button>
                 </div>
             </div>
-            <input type="text" class="input_search" v-model="search_content" />
-            <span class="search_icon"></span>
-            <button id="user" v-on:click="goto_user">Us</button>
+            <div class="search-box">
+                <input type="text" class="search-text" v-model="search_content"/>
+                <a class="search-btn" href="#"><!--这个#号要改成正确的URL，到时候有文章了可以改的-->
+                <i class="iconfont iconchazhao"></i>
+                </a>
+            </div>
+            <button id="user" v-on:click="goto_user"></button>
         </form>
+        
         <!--推荐页-->
         <div class="recommand_board">
             <div class="recommand_li" v-for="(id, index) in recommand_list">
@@ -104,7 +112,7 @@ export default {
                     return item != index;
                 })
                 var com = document.getElementById(el);
-                com.style.backgroundColor = "rgb(7, 197, 7)";
+                com.style.backgroundColor = "#18c870";
             }
             console.log(this.c_tags);
         }, recommandid(index) {
@@ -126,6 +134,10 @@ export default {
             setTimeout(function () {
                 this.$router.push({ name: "webs", params: { id: index } });
             }.bind(this), 1000)
+        }, goto_write() {
+            setTimeout(function () {
+                this.$router.push({ name: "write", params: { uid: this.uid } });
+            }.bind(this), 1000)
         }, goto_user() {
             setTimeout(function () {
                 this.$router.push({ name: "user", params: { uid: this.uid } });
@@ -143,62 +155,122 @@ export default {
     background-size: cover;
     z-index: 1000000;
 }
-
+.back{
+    /* height: 100%; */
+    /* width: 100%; */
+    background: url("../../picture/background.jpg") no-repeat;
+    /* background-size: cover; */
+    background-position: center;
+    height: 100%;
+    width: 100%;
+    background-size: cover;
+    position: fixed;
+}
 * {
     margin: 0;
     padding: 0
 }
 
 #logo {
-    margin-top: 15px;
-    height: 50px;
-    width: 100px;
-    border-radius: 50px;
+    margin-top: 10px;
+    margin-left: 10px;
+    height: 60px;
+    width: 80px;
+    border-radius: 10px;
     position: fixed;
-    background-color: white;
+
+} 
+/*右上角小人图标(117, 230, 216*/
+#user {
+    background: url(../../picture/person.png) no-repeat;
+    background-size: contain;
+    margin-top: 15px;
+    margin-left: 1480px;
+    height: 30px;
+    width: 25px;
+    border-radius: 10px;
+    position: fixed;
+    background-color: rgb(153, 227, 219);
     text-align: center;
     font-size: 35px;
 }
 
-#user {
-    margin-top: 15px;
-    margin-left: 1400px;
-    height: 50px;
+.search-box
+{
+    position: absolute;
+    margin-top: 380px;
+    margin-left: 765px;
+    transform: translate( -50%,-50%);
+    background: #eff1eb;
+    height: 40px;
+    border-radius: 30px; /*圆角边框*/
+    padding: 10px;
+}
+.search-box:hover> .search-text
+{
+    width: 400px;
+}
+.search-box:hover> .search-btn
+{
+    background: rgb(3, 6, 0);
+    color: #f0f1f3;
+}
+.search-btn
+{
+
+    color: rgb(255, 255, 255);
+    /* float: right; */
+    width: 40px;
+    height: 40px;
+    border-radius: 50%;
+    background:#18c870;
+    display: flex;
+    justify-content: center;
+    align-items: center;
+    transition: 0.5s;
+}
+.search-text
+{
+    border: none;
+    background: none;
+    outline: none;
+    float: left;
+    padding: 0;
+    color: rgb(0, 1, 9);
+    font-size: 20px;
+    font-family: "Microsoft YaHei UI Light";
+    transition: 0.5s;
+    line-height:40px;
+    width:  0px;
+
+}
+
+/* Tags*/ 
+.tag_button {
+    
+    border:none;
+    margin-top: 430px;
+    margin-left: 740px;
+    height: 30px;
     width: 50px;
     border-radius: 50px;
     position: fixed;
-    background-color: white;
+    background-color: #2fa415;
     text-align: center;
-    font-size: 35px;
 }
-
-.input_search {
-    border-color: white;
-    margin-top: 15px;
-    margin-left: 620px;
-    height: 50px;
-    width: 80px;
-    border-radius: 50px;
+#tag_list {
+    
+    height: 180px;
+    width: 430px;
+    margin-top: 500px;
+    margin-left: 550px;
+    background-color: rgb(255, 255, 255);
     position: fixed;
-    background-color: white;
-    text-align: center;
-    font-size: 25px;
-    transition: width .5s;
-    transition-delay: .1s;
+    border-radius: 20px;
+    border: solid;
+    border-color: burlywood;
+    background: url(../../picture/button_board.png) no-repeat;
 }
-
-.tag_button {
-    border-color: white;
-    margin-top: 15px;
-    margin-left: 500px;
-    height: 50px;
-    width: 100px;
-    border-radius: 50px;
-    position: fixed;
-    background-color: rgb(7, 197, 7);
-    text-align: center;
-}
-
 .ctag_button {
     border-color: white;
     margin-top: 15px;
@@ -207,7 +279,7 @@ export default {
     width: 80px;
     border-radius: 30px;
     position: fixed;
-    background-color: rgb(7, 197, 7);
+    background-color: #18c870;
     text-align: center;
 }
 
@@ -217,7 +289,7 @@ export default {
 }
 
 .button_name {
-    font-size: 25px;
+    font-size: 15px;
     color: white;
 }
 
@@ -238,16 +310,5 @@ export default {
     cursor: pointer;
 }
 
-#tag_list {
-    margin-top: 90px;
-    height: 200px;
-    width: 430px;
-    margin-left: 500px;
-    background-color: rgb(255, 255, 255);
-    position: fixed;
-    border-radius: 20px;
-    border: solid;
-    border-color: burlywood;
-    background: url(../../picture/button_board.png) no-repeat;
-}
+
 </style>
